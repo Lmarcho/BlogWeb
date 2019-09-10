ca0fcf4f9e510d0585171a565440688cc3ca542f
Lmarcho <lmathngadeera@gmail.com>
2019-09-10 21:47:50 +0530

    create login,register pages and funtions

--- BlogWeb/app.py
+++ BlogWeb/app.py
@@ -3,8 +3,10 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for, flash, redirect
+from form import RegisterFrom,LoginFrom
 app = Flask(__name__)
+app.config['SECRET_KEY']='20476bc8ecf00cebf5ea68c052af4947'
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
@@ -28,7 +30,6 @@ posts = [
 ]
 
 
-
 @app.route('/')
 @app.route('/home')
 def home():
@@ -37,6 +38,26 @@ def home():
 @app.route('/about')
 def about():
         return render_template('about.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+        form = RegisterFrom()
+        if form.validate_on_submit():
+            flash(f'Account created for {form.username.data}!','success')
+            return redirect(url_for('home'))
+        return render_template('register.html',title='Register',form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+        form = LoginFrom()
+        if form.validate_on_submit():
+            if form.email.data ==  'admin@blog.com' and form.password.data == 'password':
+                flash('You have been logged in!', 'success')
+                return redirect(url_for('home'))
+            else :
+                flash('Login Unsuccessful. Please check username and password', 'error')
+        return render_template('login.html',title='Login',form=form)
+
 
 
 if __name__ == '__main__':
